109a52e2f423cee1e95260eb2c06ae95f233b0b8
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 02:00:36 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  def initialize(options)
+    @name = options[:name]
+    @weight = options[:weight]
+    @height = options[:height]
+  end
 end
