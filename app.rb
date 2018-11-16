9c8b44bc1d1646bd5d4e4f16400f93b427705bf2
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 02:16:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
       @ships = params[:pirate][:ships].map { |ship| Ship.new(ship) }
+      binding.pry
       erb :"pirates/show"
     end
   end
